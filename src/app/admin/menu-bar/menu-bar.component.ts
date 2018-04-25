5bba77bfb66148e8a32f5dec0cadcaf528d1de74
Rohit <rohit157.rg@gmail.com>
2018-04-25 21:22:09 +0530

    Signed-off-by: Rohit <rohit157.rg@gmail.com>

---
+++ src/app/admin/menu-bar/menu-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.css']
+})
+export class MenuBarComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+  navigate(route:string) {
+  	this.router.navigate([route]);
+  }
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { MenuBarComponent } from './admin/menu-bar/menu-bar.component';
+import { FooterComponent } from './admin/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MenuBarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
